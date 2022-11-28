312e7ae8012418b3a65402f280556b44ffb37908
Tanja Veljanovska <tanja.veljan@gmail.com>
2022-11-28 21:09:43 +0100

    Create new action, view and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # Read from CRUD
+  def show
+    @article = Article.find(params[:id])
+  end
 end
