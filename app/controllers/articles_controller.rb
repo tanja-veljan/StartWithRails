80399bc7cc420406495096d43cf03276a7727e51
Tanja Veljanovska <tanja.veljan@gmail.com>
2022-11-28 21:34:07 +0100

    Create new controler actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # Create step 1 from CRUD
+  def new
+    @article = Article.new
+  end
+
+  # Create step 2 from CRUD
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
