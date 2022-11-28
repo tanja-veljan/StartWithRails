3e6d1a1623ca18343fc1e24faf40ab92c812785a
Tanja Veljanovska <tanja.veljan@gmail.com>
2022-11-28 21:24:15 +0100

    Use article_path helper

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,17 @@ Rails.application.routes.draw do
 
   resources :articles
 end
+
+=begin
+$ bin/rails routes
+      Prefix Verb   URI Pattern                  Controller#Action
+        root GET    /                            articles#index
+    articles GET    /articles(.:format)          articles#index
+ new_article GET    /articles/new(.:format)      articles#new
+     article GET    /articles/:id(.:format)      articles#show
+             POST   /articles(.:format)          articles#create
+edit_article GET    /articles/:id/edit(.:format) articles#edit
+             PATCH  /articles/:id(.:format)      articles#update
+             DELETE /articles/:id(.:format)      articles#destroy
+=end
+
